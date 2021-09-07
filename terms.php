3b4705fe70c3948789961fe09d0294109d5120be
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-09-07 14:16:01 +0100

    added terms page

---
+++ terms.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Smell Of Football 2</title>
+    <link type="text/css" rel="stylesheet" href="/assets/css/reset.css?v=<?= rand() ?>">
+    <link type="text/css" rel="stylesheet" href="/assets/css/index.css?v=<?= rand() ?>">
+</head>
+
+<body>
+    <header class="c-header">
+        <div class="c-header__wrapper l-restrict">
+            <div class="c-socials">
+                <!-- Facebook Logo --><a href="/"><img src="/assets/graphics/facebook.svg" alt="Facebook Logo"></a>
+                <!-- Twitter Logo --><a href="/"><img src="/assets/graphics/twitter.svg" alt="Twitter Logo"></a>
+                <!-- Instagram Logo --><a href="/"><img src="/assets/graphics/instagram.svg" alt="Instagram Logo"></a>
+            </div>
+            <img src="/assets/graphics/logo.svg" alt="The Smell Of Football 2" class="c-header__logo">
+            <div class="c-header__buttons">
+                <a href="/"><img class="c-shopping-cart" src="/assets/graphics/cart.svg" alt="cart"></a>
+                <div class="c-hamburger__wrapper" id="hamburger-container">
+                    <button class="c-hamburger-button" id="hamburger-button" aria-label="menu">
+                        <div class="c-hamburger-button__line"></div>
+                        <div class="c-hamburger-button__line"></div>
+                        <div class="c-hamburger-button__line"></div>
+                    </button>
+                    <nav class="c-hamburger" id="hamburger-menu">
+                        <a href="/">Home</a>
+                        <a href="/contact">Contact</a>
+                        <a href="/terms">Terms and Conditions</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="l-block l-block--padding">
+            <div class="l-restrict l-restrict--narrow">
+                <h2>Terms and Conditions</h2>
+
+            </div>
+        </section>
+    </main>
+    <footer class="l-block c-footer">
+        <div class="l-restrict c-footer__wrapper">
+            <div class="c-logos">
+                <!-- Twitter Logo --><a href="/"><img src="/assets/graphics/linkedin.svg" alt="Linkedin Logo"></a>
+                <!-- Instagram Logo --><a href="/"><img src="/assets/graphics/twitter white.svg" alt="Twitter Logo"></a>
+                <!-- Facebook Logo --><a href="/"><img src="/assets/graphics/facebook white.svg" alt="Facebook Logo"></a>
+            </div>
+            <p class="c-copywrite">
+                &copy; Mick Rathbone 2021
+            </p>
+        </div>
+    </footer>
+
+</body>
+<script src="/assets/js/index.js?v=<?= rand() ?>" type="module"></script>
+<script src="https://kit.fontawesome.com/5bcd9efe81.js" crossorigin="anonymous"></script>
+
+</html>
