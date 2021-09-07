35f9acfee96de1a6134e6c33be7169c9ad59be4b
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-09-07 13:37:47 +0100

    implemented new design

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,70 +9,89 @@
     <link type="text/css" rel="stylesheet" href="/assets/css/reset.css?v=<?= rand() ?>">
     <link type="text/css" rel="stylesheet" href="/assets/css/index.css?v=<?= rand() ?>">
 </head>
+
 <body>
     <header class="c-header">
         <div class="c-header__wrapper l-restrict">
             <div class="c-socials">
-                <!-- Facebook Logo --><i class="fab fa-facebook-square"></i>
-                <!-- Twitter Logo --><i class="fab fa-twitter-square"></i>
-                <!-- Instagram Logo --><i class="fab fa-instagram"></i>
+                <!-- Facebook Logo --><a href="/"><img src="/assets/graphics/facebook.svg" alt="Facebook Logo"></a>
+                <!-- Twitter Logo --><a href="/"><img src="/assets/graphics/twitter.svg" alt="Twitter Logo"></a>
+                <!-- Instagram Logo --><a href="/"><img src="/assets/graphics/instagram.svg" alt="Instagram Logo"></a>
             </div>
-            <div class="c-header__title-wrapper">
-                <h1 class="c-header__title">THE SMELL OF<br><span class="c-header__title--large">FOOTBALL</span></h1>
-                <div class="c-header__title-number-wrapper">
-                    <h1 class="c-header__title-number">2</h1>
+            <img src="/assets/graphics/logo.svg" alt="The Smell Of Football 2" class="c-header__logo">
+            <div class="c-header__buttons">
+                <a href="/"><img class="c-shopping-cart" src="/assets/graphics/cart.svg" alt="cart"></a>
+                <div class="c-hamburger__wrapper" id="hamburger-container">
+                    <button class="c-hamburger-button" id="hamburger-button" aria-label="menu">
+                        <div class="c-hamburger-button__line"></div>
+                        <div class="c-hamburger-button__line"></div>
+                        <div class="c-hamburger-button__line"></div>
+                    </button>
+                    <nav class="c-hamburger" id="hamburger-menu">
+                        <a href="/">Home</a>
+                        <a href="/about">About</a>
+                        <a href="/contact">Contact</a>
+                    </nav>
                 </div>
-            </div>
-            <div class="c-hamburger__wrapper" id="hamburger-container">
-                <button class="c-hamburger-button" id="hamburger-button" aria-label="menu">
-                    <div class="c-hamburger-button__line"></div>
-                    <div class="c-hamburger-button__line"></div>
-                    <div class="c-hamburger-button__line"></div>
-                </button>
-                <nav class="c-hamburger" id="hamburger-menu">
-                    <a href="/">Home</a>
-                    <a href="/about">About</a>
-                    <a href="/contact">Contact</a>
-                </nav>
             </div>
         </div>
     </header>
+    <section class="l-block c-purchase__background">
+        <div class="l-restrict c-purchase">
+            <h3>GET YOUR COPY NOW - £14.99</h3>
+            <div class="c-purchase__options">
+                <div class="c-purchase__counter">
+                    <div class="c-purchase__counter-button">-</div>
+                    <div class="c-purchase__counter-count">0</div>
+                    <div class="c-purchase__counter-button">+</div>
+                </div>
+                <button class="c-button c-purchase__purchase-button">Add To Basket</button>
+                <p>(P&P £2.99)</p>
+            </div>
+        </div>
+    </section>
     <section class="l-block">
         <div class="l-restrict c-product">
-            <div class="c-product__details">
-                <p class="c-product__description">
-                    Perunti omnimusa nobis dollorissim que dia volor
-                    aperit velia eat et, quis volorum aut facest, quo
-                    ium non re, explibus dolupti nessumque volupta
-                    que sundes doloria ditibus am as molesti onsequi
-                    illabor eptatem aperiae. Uptatemp oratur, sin
-                    expelit inulpa voluptae. Ut opta nonsequunt
-                    simet essint ut unt, tem ut ius.
-                </p>
-                <div class="c-product__price">
-                    £14.99
+            <div class="c-product__quotes">
+                <div class="c-quote">
+                    <p class="c-quote__text">
+                        “A book that encapsulates the author - laugh
+                        out load funny one moment, poignant the next
+                        but always with a zest for life and a profound
+                        love for an industry like no other: a must read.”
+                    </p>
+                    <p class="c-quote__author">
+                        Dominic King
+                    </p>
+                    <p class="c-quote__paper">
+                        Daily Mail
+                    </p> 
                 </div>
-                <div class="c-product__purchase-wrapper">
-                    <div class="c-product__purchase">
-                        <div class="c-product__counter">
-                            <div class="c-product__counter-button">-</div>
-                            <div class="c-product__counter-count">0</div>
-                            <div class="c-product__counter-button">+</div>
-                        </div>
-                        <button class="c-button c-product__purchase-button">Add To Basket</button>
-                    </div>
-                    <div class="c-payment-types">
-                        <!-- Amex --><i class="fab fa-cc-amex"></i>
-                        <!-- Apple Pay --><i class="fab fa-cc-apple-pay"></i>
-                        <!-- Mastercard --><i class="fab fa-cc-mastercard"></i>
-                        <!-- Something Pay --><div class="c-payment-type__placeholder"></div>
-                        <!-- Visa --><i class="fab fa-cc-visa"></i>
-                    </div>
+                <div class="c-quote">
+                    <p class="c-quote__text">
+                        “Forget The Empire Strikes Back, Godfather 2,
+                        even From Russia With Love (and I love From
+                        Russia With Love) but Baz Rathbone’s The Smell
+                        Of Football 2 is undeniably the greatest sequel
+                        ever written. The Smell Of Football was very
+                        highly acclaimed. This is even better - an older
+                        man’s battle to prove himself relevant in a
+                        young man’s world, it is soul bearingly honest,
+                        insightful, wildly entertaining and laugh
+                        out loud funny. It is poignant, perceptive and
+                        utterly charming. And guess what? If you think
+                        no-one can beat the march of time, think again.
+                        Passionately recommended.”
+                    </p>
+                    <p class="c-quote__author">
+                        David Prentice
+                    </p>
+                    <p class="c-quote__paper">
+                        Liverpool Echo Sports Editor
+                    </p> 
                 </div>
             </div>
-            <div class="c-product__image-wrapper">
-                <div class="c-product__image-placeholder"></div>
-            </div>
+            <img class="c-product__image" src="/assets/images/book_small.jpg" alt="The Smell Of Football 2 Book">
         </div>
     </section>
     <section class="l-block c-banner">
@@ -80,56 +100,60 @@
             <h4>MICK ‘BAZ’ RATHBONE</h4>
         </div>
     </section>
-    <section class="l-block c-testimonials">
-        <div class="l-restrict c-testimonials__wrapper">
-            <div class="c-testimonials__images"></div>
-            <div class="c-testimonials__quotes">
-                <div class="c-testimonials__quote">
-                    <p class="c-testimonials__quote-text">"THE BEST FOLLOW UP BOOK IN THE WORLD OF FOOTBALL!"</p>
-                    <p class="c-testimonials__credit">DAILY MIRROR</p>
-                </div>
-                <div class="c-testimonials__quote">
-                    <p class="c-testimonials__quote-text">"THE BEST FOLLOW UP BOOK IN THE WORLD OF FOOTBALL!"</p>
-                    <p class="c-testimonials__credit">DAILY MIRROR</p>
-                </div>
-                <div class="c-testimonials__quote">
-                    <p class="c-testimonials__quote-text">
-                        “RATHBONE’S FOLLOW
-                        ON AUTOBIOGRAPHY IS A
-                        SUPERB READ”
-                    </p>
-                    <p class="c-testimonials__credit">THE SUN</p>
-                </div>
+    <section class="l-block c-images">
+        <div class="restrict c-images__container">
+            <div class="c-images__image">
+                <img src="/assets/images/300w/IMG_0044_300w.jpg" alt="Mick Rathbone">
+            </div>
+            <div class="c-images__image">
+                <img src="/assets/images/300w/IMG_0087_300w.JPG" alt="Training on the beach">
+            </div>
+            <div class="c-images__image">
+                <img src="/assets/images/300w/IMG_0467_300w.JPG" alt="The team">
+            </div>
+            <div class="c-images__image">
+                <img src="/assets/images/300w/IMG_0484_300w.JPG" alt="Football Pitch">
             </div>
         </div>
     </section>
     <section class="l-block c-bio">
-        <div class="l-restrict l-restrict--narrow">
+        <div class="l-restrict l-restrict--narrow c-bio__wrapper">
+            <h3>ABOUT THE AUTHOR</h3>
             <p class="c-bio__text">
-                When Mick Rathbone signed for Birmingham City as a 16 year-old apprentice
-                he was living every schoolboy’s dream. But when he discovered he was so
-                nervous he was unable to speak, let alone pass the ball, in the presence of his
-                boyhood hero and City star Trevor Francis. That’s when he realised that a career
-                in football might not be everything he had imagined. The Smell of Football is
-                the brutally honest and utterly unputdownable story of how ‘Baz’ conquered
-                his personal demons to build a life in the game.
+            Mick (Baz) Rathbone was born in Birmingham in 1958. Highly academic, he was determined to become a Doctor.
+            He was also an outstanding long distance runner and footballer. Against the advice of his father and headmaster,
+            he left school at 16 years of age to sign apprentice professional forms for his local football club, Birmingham City
+            F.C. Things went very well at first and he was selected to play for the England Youth Team. He also made his senior
+            league debut at Tottenham that same season at only 17 years of age. He had the world at his feet. Great things
+            were predicted for this lad from Sheldon. Unfortunately, things started to go wrong and the strain of playing top flight
+            football, as a teenager, for the club that he had supported, became too much. His career imploded and it reached
+            the point where he didn’t want to play football any more. Luckily, he went on loan to Blackburn Rovers in 1979 and
+            the physical separation from his home town allowed him to have a relatively successful lower league career. There
+            were still many challenges along the way but eventually he became a Chartered Physiotherapist, working his way
+            up from the lower leagues, to becoming head of the medical department at Everton F.C. in 2002, at 44 years of
+            age. He enjoyed a successful 8 years at Everton. When he left in 2010 he wrote a much acclaimed book called <strong>The
+            Smell Of Football</strong>, which spoke openly about his struggles with self confidence. The book was highly successful
+            and was nominated for several prestigious literary awards. The much anticipated sequel; <strong>The Smell Of Football 2</strong>
+            has now been written and is receiving similar accolades. It covers 11 years and 10 different football teams and deals
+            with the issues of getting older in a sport fixated on youth.
             </p>
         </div>
     </section>
     <footer class="l-block c-footer">
         <div class="l-restrict c-footer__wrapper">
             <div class="c-logos">
-                <i class="fab fa-facebook"></i>
-                <div class="c-logos__logo-placeholder"></div>
-                <div class="c-logos__logo-placeholder"></div>
+                <!-- Twitter Logo --><a href="/"><img src="/assets/graphics/linkedin.svg" alt="Linkedin Logo"></a>
+                <!-- Instagram Logo --><a href="/"><img src="/assets/graphics/twitter white.svg" alt="Twitter Logo"></a>
+                <!-- Facebook Logo --><a href="/"><img src="/assets/graphics/facebook white.svg" alt="Facebook Logo"></a>
             </div>
-            <div class="c-copywrite">
+            <p class="c-copywrite">
                 &copy; Mick Rathbone 2021
-            </div>
+            </p>
         </div>
     </footer>
-    
+
 </body>
 <script src="/assets/js/index.js?v=<?= rand() ?>" type="module"></script>
 <script src="https://kit.fontawesome.com/5bcd9efe81.js" crossorigin="anonymous"></script>
+
 </html>
