2a941b085886ba0f04c0047fd29d12bcc43bb6c8
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-11-11 14:39:34 +0000

    added decode to getAddress

--- event-handler.php
+++ event-handler.php
@@ -3,7 +3,8 @@
 require 'vendor/autoload.php';
 require 'config.php';
 
-function isLocalhost($whitelist = ['127.0.0.1', '::1']) {
+function isLocalhost($whitelist = ['127.0.0.1', '::1'])
+{
     return in_array($_SERVER['REMOTE_ADDR'], $whitelist);
 }
 
@@ -14,44 +15,42 @@ $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
 $event = null;
 
 try {
-  $event = \Stripe\Webhook::constructEvent(
-    $payload, $sig_header, $endpoint_secret
-  );
-} catch(\UnexpectedValueException $e) {
-  // Invalid payload
-  http_response_code(400);
-  exit();
-} catch(\Stripe\Exception\SignatureVerificationException $e) {
-  // Invalid signature
-  http_response_code(400);
-  exit();
+    $event = \Stripe\Webhook::constructEvent(
+        $payload,
+        $sig_header,
+        $endpoint_secret
+    );
+} catch (\UnexpectedValueException $e) {
+    // Invalid payload
+    http_response_code(400);
+    exit();
+} catch (\Stripe\Exception\SignatureVerificationException $e) {
+    // Invalid signature
+    http_response_code(400);
+    exit();
 }
 
-function getAddress(\Stripe\PaymentIntent $paymentIntent) {
-    // try {
-    //     $addressObject = $paymentIntent["shipping"]["address"];
-    //     $line1 = $addressObject["line1"];
-    //     $line2 = $addressObject["line2"];
-    //     $city = $addressObject["city"];
-    //     $country = $addressObject["country"];
-    //     $postCode = $addressObject["postal_code"];
-    //     $state = $addressObject["state"];
-    //     return `
-    //     $addressObject<br>
-    //     $line1<br>
-    //     $line2<br>
-    //     $city<br>
-    //     $country<br>
-    //     $postCode<br>
-    //     $state<br>
-    //     `;
-    // } catch (Exception $e) {
-    //     return $e->getMessage();
-    // }
-    return "Hello There";
+function getAddress(\Stripe\PaymentIntent $paymentIntent)
+{
+    $addressObject = json_decode($paymentIntent["shipping"]["address"], true);
+    $line1 = $addressObject["line1"];
+    $line2 = $addressObject["line2"];
+    $city = $addressObject["city"];
+    $country = $addressObject["country"];
+    $postCode = $addressObject["postal_code"];
+    $state = $addressObject["state"];
+    return `
+        $line1<br>
+        $line2<br>
+        $city<br>
+        $country<br>
+        $postCode<br>
+        $state<br>
+        `;
 }
 
-function handlePaymentIntent(\Stripe\PaymentIntent $paymentIntent) {
+function handlePaymentIntent(\Stripe\PaymentIntent $paymentIntent)
+{
     $headers = ["Content-type: text/html; charset=iso-8859-1"];
 
     $fields = [];
@@ -72,13 +71,13 @@ function handlePaymentIntent(\Stripe\PaymentIntent $paymentIntent) {
 
 // Handle the event
 switch ($event->type) {
-  case 'payment_intent.succeeded':
-    $paymentIntent = $event->data->object;
-    handlePaymentIntent($paymentIntent);
-    break;
-  // ... handle other event types
-  default:
-    echo 'Received unknown event type ' . $event->type;
+    case 'payment_intent.succeeded':
+        $paymentIntent = $event->data->object;
+        handlePaymentIntent($paymentIntent);
+        break;
+        // ... handle other event types
+    default:
+        echo 'Received unknown event type ' . $event->type;
 }
 
 http_response_code(200);
