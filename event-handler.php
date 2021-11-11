ffa16def40205acc9ce051dab7a96329efe54d0d
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-11-11 15:21:48 +0000

    added items table

--- event-handler.php
+++ event-handler.php
@@ -50,6 +50,30 @@ function getAddress(\Stripe\PaymentIntent $paymentIntent)
         ";
 }
 
+function formatPrice($price) {
+    if (strlen($price) == 2) {
+        $price .= ".00";
+    } else if (strlen($price) == 3) {
+        $price .= "0";
+    }
+    return $price;
+}
+
+function getItems($items) {
+    $itemTable = "<table><thead><tr><th style='font-weight: bold;'>Item</th><thstyle='font-weight: bold;'>Quantity</th><th style='font-weight: bold;'>Price</th><th style='font-weight: bold;'>Total</th></tr></thead>";
+    foreach ($items as $item) {
+        $price = $item["amount"]/100;
+        $price = formatPrice($price);
+
+        $total = ($item["amount"] * $item["quantity"])/100;
+        $total = formatPrice($total);
+
+        $itemTable .= "<tr><td>$item[description]</td><td>$item[quantity]</td><td>$price</td><td>$total</td></tr>";
+    }
+    $itemTable .= "</table>";
+    return $itemTable;
+}
+
 function handlePaymentIntent(\Stripe\PaymentIntent $paymentIntent)
 {
     $headers = ["Content-type: text/html; charset=iso-8859-1"];
@@ -57,7 +81,24 @@ function handlePaymentIntent(\Stripe\PaymentIntent $paymentIntent)
     $fields = [];
     $fields["Name"] = $paymentIntent["shipping"]["name"];
     $fields["Address"] = getAddress($paymentIntent);
-    $fields["Paid"] = 
+
+    $message = "<table cellspacing='1' cellpadding='6' border='1'>";
+
+    foreach ($fields as $key => $value) {
+        $message .= "<tr><td valign='top' style='font-weight: bold;'>$key</td><td>$value</td></tr>";
+    }
+
+    $message .= "</table>";
+    mail(EMAIL, "Webhook", $message, implode("\r\n", $headers));
+}
+
+function handleOrder($order) {
+    $headers = ["Content-type: text/html; charset=iso-8859-1"];
+
+    $fields = [];
+    $fields["Name"] = $order["shipping"]["name"];
+    $fields["Address"] = getAddress($order);
+    $fields["Items"] = getItems($order["items"]);
 
     $message = "<table cellspacing='1' cellpadding='6' border='1'>";
 
