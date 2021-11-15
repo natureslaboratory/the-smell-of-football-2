f85d9390a356c1a9205af089c8fa914122fe1282
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-11-15 16:43:05 +0000

    added matmoto code

--- components/footer.php
+++ components/footer.php
@@ -13,5 +13,21 @@
 </body>
 <script src="/assets/js/index.js?v=<?= rand() ?>" type="module"></script>
 <script src="https://kit.fontawesome.com/5bcd9efe81.js" crossorigin="anonymous"></script>
+<!-- Matomo -->
+
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://analytics.natureslaboratory.co.uk/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '14']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 
 </html>
